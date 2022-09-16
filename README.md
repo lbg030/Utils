5450e075df73a47f69d8874204a0238625cbf22c
ByeongGwon <lee971009@naver.com>
2022-09-16 11:16:51 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Utils
+ 파이썬 Utils 파일 모음
---
+++ coutingfiles.py
@@ -0,0 +1,19 @@
+import os
+root = "zipper"
+files = {}
+walk = [root]
+tmp = []
+while walk:
+    folder = walk.pop(0)+"/"
+    items = os.listdir(folder) # items = folders + files
+    for i in items:
+        i=folder+i
+        (walk if os.path.isdir(i) else tmp).append(i)
+    files[folder] = tmp
+    tmp = []
+print(files.keys())
+# print(files.values())
+for x in files.keys():
+    if len(files[x]):
+        print(x)
+        print(len(files[x]))
