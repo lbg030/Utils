b2753abddc92e5d51216cbc688177dfb77e5e0d9
ByeongGwon <lee971009@naver.com>
2022-10-26 16:05:09 +0900

    split file commit

--- train_test_split.py
+++ train_test_split.py
@@ -1,23 +1,50 @@
 import os
 import shutil
 
-dir_name = "/Users/ibyeong-gwon/Desktop/data"
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
+ 
+
+
+
+dir_name = "/Users/ibyeong-gwon/Desktop/fst/data"
 # dir_list = os.listdir(dir_name)
 move_dir_name = ""
 defect_list = ['yd', 'hz', 'bz', 'chem']
+
+defect_dict = {'hz':253,
+                'bz':29,
+                'chem':66,
+                'yd':52}
+
 for defect in defect_list :
     cnt = 0 # Random Sampling 용
     file_list = os.listdir(f"{dir_name}/{defect}/raw_image")
-    move_dir_name = f"{dir_name}/{defect}/"
+    # move_dir_name = f"{dir_name}/{defect}/"
+    move_dir_name = f"/Users/ibyeong-gwon/Desktop/fst/{defect}/"
     file_len = len(file_list)
     
-    split_point = int(file_len * 0.8)
+    split_point = defect_dict[defect]-1
     
-    for file in file_list[:split_point]:
-        shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "train")
-        if cnt < 100:
-            shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "cls_train")
-            cnt += 1
+    # for file in file_list[:split_point]:
+    #     shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "train")
+    #     if cnt < 100:
+    #         shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "cls_train")
+    #         cnt += 1
         
-    for file in file_list[split_point:]:
-        shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "test")
+    # for file in file_list[split_point:]:
+    #     shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "test")
+
+    num = 5
+    for file in file_list[:split_point]:
+        try :
+            createFolder(f'/Users/ibyeong-gwon/Desktop/fst/{defect}/cls_train{num}')
+        except:
+            print("Error")
+            
+        shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + f"cls_train{num}")
+        
