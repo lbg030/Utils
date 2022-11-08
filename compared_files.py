ae0238ba567740485959a84a8bbbed82ebad5d84
ByeongGwon <lee971009@naver.com>
2022-11-08 16:52:54 +0900

    compared

--- compared_files.py
+++ compared_files.py
@@ -2,13 +2,14 @@ import os
 import shutil
 
 # 400장 파일
-path = "/Users/ibyeong-gwon/Desktop/data/fst/yd/images"
+path = "/Users/ibyeong-gwon/Desktop/test"
 lst = os.listdir(path)
 
 #원본 파일
-path = "/Users/ibyeong-gwon/Desktop/check/yd/raw_image"
+path = "/Users/ibyeong-gwon/Desktop/lbg"
 lst2 = os.listdir(path)
 
 for x in lst:
-    if x not in lst2:
+    if x not in lst2 :
         print(x)
+        
