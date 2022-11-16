7f7fb979f3103dd93aacd756fecb48eb2a5c2c8e
ByeongGwon <lee971009@naver.com>
2022-11-16 09:10:51 +0900

    1

--- compared_files.py
+++ compared_files.py
@@ -1,15 +1,21 @@
 import os
 import shutil
 
-# 400장 파일
-path = "/Users/ibyeong-gwon/Desktop/test"
-lst = os.listdir(path)
+# # 400장 파일
+# path = "/Users/ibyeong-gwon/Desktop/test"
+# lst = os.listdir(path)
 
-#원본 파일
-path = "/Users/ibyeong-gwon/Desktop/lbg"
-lst2 = os.listdir(path)
+# #원본 파일
+# path = "/Users/ibyeong-gwon/Desktop/lbg"
+# lst2 = os.listdir(path)
 
-for x in lst:
-    if x not in lst2 :
-        print(x)
+
         
+    
+# cnt = 0
+# for x in lst2:
+#     if x not in lst:
+#         print(x)
+#         cnt += 1
+        
+# print(cnt)
--- train_test_split.py
+++ train_test_split.py
@@ -1,57 +1,57 @@
-import os
-import shutil
+# import os
+# import shutil
 
-def createFolder(directory):
-    try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    except OSError:
-        print ('Error: Creating directory. ' +  directory)
+# def createFolder(directory):
+#     try:
+#         if not os.path.exists(directory):
+#             os.makedirs(directory)
+#     except OSError:
+#         print ('Error: Creating directory. ' +  directory)
  
-## hz bz chem yd
-## 1732 448 330 169 
-## 12 3.5 3 1.5 -> 20
+# ## hz bz chem yd
+# ## 1732 448 330 169 
+# ## 12 3.5 3 1.5 -> 20
 
-## -> 
+# ## -> 
 
 
 
-dir_name = "/Users/ibyeong-gwon/Desktop/custom_data"
-# dir_list = os.listdir(dir_name)
-move_dir_name = ""
-defect_list = ['yd', 'hz', 'bz', 'chem']
+# dir_name = "/Users/ibyeong-gwon/Desktop/custom_data"
+# # dir_list = os.listdir(dir_name)
+# move_dir_name = ""
+# defect_list = ['yd', 'hz', 'bz', 'chem']
 
-defect_dict = {'hz':120,
-                'bz':35,
-                'chem':30,
-                'yd':15}
+# defect_dict = {'hz':120,
+#                 'bz':35,
+#                 'chem':30,
+#                 'yd':15}
 
-for defect in defect_list :
-    cnt = 0 # Random Sampling 용
-    file_list = os.listdir(f"{dir_name}/{defect}/train")
-    # move_dir_name = f"{dir_name}/{defect}/"
-    move_dir_name = f"/Users/ibyeong-gwon/Desktop/custom_data/cls/{defect}/"
-    file_len = len(file_list)
+# for defect in defect_list :
+#     cnt = 0 # Random Sampling 용
+#     file_list = os.listdir(f"{dir_name}/{defect}/train")
+#     # move_dir_name = f"{dir_name}/{defect}/"
+#     move_dir_name = f"/Users/ibyeong-gwon/Desktop/custom_data/cls/{defect}/"
+#     file_len = len(file_list)
     
-    num = 5
-    split_point = defect_dict[defect] * num
+#     num = 5
+#     split_point = defect_dict[defect] * num
     
-    print(defect, split_point)
-    # for file in file_list[:split_point]:
-    #     shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "train")
-    #     if cnt < 100:
-    #         shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "cls_train")
-    #         cnt += 1
+#     print(defect, split_point)
+#     # for file in file_list[:split_point]:
+#     #     shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "train")
+#     #     if cnt < 100:
+#     #         shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "cls_train")
+#     #         cnt += 1
         
-    # for file in file_list[split_point:]:
-    #     shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "test")
+#     # for file in file_list[split_point:]:
+#     #     shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "test")
 
-    
-    for file in file_list[:split_point]:
-        try :
-            createFolder(f'/Users/ibyeong-gwon/Desktop/custom_data/cls/{defect}/cls_train{num}')
-        except:
-            print("Error")
+#     for file in file_list[:split_point]:
+#         try :
+#             createFolder(f'/Users/ibyeong-gwon/Desktop/custom_data/cls/{defect}/cls_train{num}')
+#         except:
+#             print("Error")
             
-        shutil.copy(f"{dir_name}/{defect}/train/{file}", move_dir_name + f"cls_train{num}")
-        
+#         shutil.copy(f"{dir_name}/{defect}/train/{file}", move_dir_name + f"cls_train{num}")
+a = 0.1
+print(a == 0.1)
