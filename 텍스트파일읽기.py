3558ae5e843d59cd54f342c4b91b06426ba6d067
ByeongGwon <lee971009@naver.com>
2022-12-29 13:45:54 +0900

    쓸모 없는 파일들 remove

--- 텍스트파일읽기.py
+++ 텍스트파일읽기.py
@@ -1,10 +1,3 @@
-# from glob import glob
-# train_img_list = glob('/Users/ibyeong-gwon/Desktop/custom_data/bz/train/*.jpg')
-
-# with open('/Users/ibyeong-gwon/Desktop/Git/Utils/test.txt', 'w') as f:
-# 	f.write('\n'.join(train_img_list) + '\n')
- 
- 
 import yaml
 with open("/Users/ibyeong-gwon/Desktop/Git/Utils/test.yaml", 'r') as f:
   data = yaml.load(f, Loader=yaml.FullLoader)
--- 특정타입remove.py
+++ 특정타입remove.py
@@ -1,12 +1,13 @@
 import os
+import shutil
 
 if __name__ == "__main__":
     cnt = 0
-    root_dir = "/Users/ibyeong-gwon/Desktop/회사"
+    root_dir = "/Users/ibyeong-gwon/Desktop/data/experiments"
     for (root, dirs, files) in os.walk(root_dir):
         if len(files) > 0:
             for file_name in files:
-                if file_name.startswith(".DS"):
+                if file_name.endswith(""): # 특정 type remove
                     file_path = root+"/"+file_name
                     try :
                         os.remove(file_path)
