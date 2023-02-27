7681a8e68257751f3040a541a9f5977bec8eb817
병권 <66514138+lbg030@users.noreply.github.com>
2023-02-27 10:44:08 +0900

    train_test_split

--- train_test_split.py
+++ train_test_split.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+from pathlib import Path 
+import argparse
 
 def createFolder(directory):
     try:
@@ -8,42 +10,80 @@ def createFolder(directory):
     except OSError:
         print ('Error: Creating directory. ' +  directory)
 
-dir_name = "/Users/ibyeong-gwon/Desktop/custom_data"
-# dir_list = os.listdir(dir_name)
-move_dir_name = ""
-defect_list = ['yd', 'hz', 'bz', 'chem']
 
-defect_dict = {'hz':120,
-                'bz':35,
-                'chem':30,
-                'yd':15}
+#TODO : Valid 까지 split 추가
+#TODO : multi defect에 관해서 한번에 처리할 수 있는 logic 필요
+#TODO : Json 추가
 
-for defect in defect_list :
-    cnt = 0 # Random Sampling 용
-    file_list = os.listdir(f"{dir_name}/{defect}/train")
-    # move_dir_name = f"{dir_name}/{defect}/"
-    move_dir_name = f"/Users/ibyeong-gwon/Desktop/custom_data/cls/{defect}/"
-    file_len = len(file_list)
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", type=str, default = "/home/lbg030/luna/data/Laser")
+    parser.add_argument("--ratio", type=float, default = 0.2, help= "Train과 Test Split 비율")
     
-    num = 5
-    split_point = defect_dict[defect] * num
+    args = parser.parse_args()
     
-    print(defect, split_point)
-    # for file in file_list[:split_point]:
-    #     shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "train")
-    #     if cnt < 100:
-    #         shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "cls_train")
-    #         cnt += 1
+    return args
+
+def main(args):
+    # dir_paths = os.listdir(args.dir)
+    defect_class = os.listdir(args.dir)
+    
+    train_paths = []
+    test_paths = []
+    
+    train_img_dir = f"{args.dir}/train/images"
+    train_label_dir = f"{args.dir}/train/labels"
+    test_img_dir = f"{args.dir}/test/images"
+    test_label_dir = f"{args.dir}/test/labels"
+    
+    createFolder(train_img_dir)
+    createFolder(train_label_dir)
+    createFolder(test_img_dir)
+    createFolder(test_label_dir)
+    
+    
+    print(defect_class)
+    
+    for defect in defect_class:
+        img_paths = []
+        for path in Path(args.dir).glob(f"{defect}/*"):
+            if path.suffix in ['.jpg', '.png']:
+                img_paths.append(str(path))
+                
+        img_paths.sort()
         
-    # for file in file_list[split_point:]:
-    #     shutil.copy(f"{dir_name}/{defect}/raw_image/{file}", move_dir_name + "test")
-
-    for file in file_list[:split_point]:
-        try :
-            createFolder(f'/Users/ibyeong-gwon/Desktop/custom_data/cls/{defect}/cls_train{num}')
-        except:
-            print("Error")
+        split_points = int(len(img_paths) * args.ratio)
+        
+        train_img = img_paths[split_points:]
+        test_img = img_paths[:split_points]
+        
+        train_paths.extend(train_img)
+        test_paths.extend(test_img)
+        
+        print(defect)
+        print(len(img_paths))
+        print(len(train_img))
+        print(len(test_img))
+        
+        for file in train_img:
+            json_file = file[:-4] + '.json'
+            txt_file = file[:-4] + '.txt'
+            # shutil.copy(f"{args.dir}/{file}", f"{train_img_dir}")
+            # shutil.copy(f"{args.dir}/{json_file}", f"{train_label_dir}")
+            shutil.copy(f"{file}", f"{train_img_dir}")
+            shutil.copy(f"{json_file}", f"{train_label_dir}")
+            shutil.copy(f"{txt_file}", f"{train_label_dir}")
             
-        shutil.copy(f"{dir_name}/{defect}/train/{file}", move_dir_name + f"cls_train{num}")
-# a = 0.1
-# print(a == 0.1)
+        for file in test_img:
+            json_file = file[:-4] + '.json'
+            txt_file = file[:-4] + '.txt'
+            
+            shutil.copy(f"{file}", f"{test_img_dir}")
+            shutil.copy(f"{json_file}", f"{test_label_dir}")
+            shutil.copy(f"{txt_file}", f"{test_label_dir}")
+        
+    
+if __name__ == "__main__":
+    args = parser()
+    main(args)
+    print("it's done")
